5c92c1a597dd1bbd359e28e7ede17232ea8d76a4
wp8de <wp8de@DESKTOP-A6F5GVH>
2018-03-07 23:53:59 +0530

    ViewPager

---
+++ HelloSwipeViewWithTabs/ContactsAdapter.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Support.V4.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Provider;
+
+namespace HelloSwipeViewWithTabs
+{
+    public class ContactsAdapter : BaseAdapter
+    {
+        List<Contact> _contactList;
+        Android.Support.V4.App.Fragment _activity;
+
+        public ContactsAdapter(Android.Support.V4.App.Fragment activity)
+        {
+            _activity = activity;
+
+            FillContacts();
+        }
+
+        public override int Count
+        {
+            get { return _contactList.Count; }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null; // could wrap a Contact in a Java.Lang.Object to return it here if needed
+        }
+
+        public override long GetItemId(int position)
+        {
+            return _contactList[position].Id;
+        }
+
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView ?? _activity.LayoutInflater.Inflate(Resource.Layout.ContactListItem, parent, false);
+            var contactName = view.FindViewById<TextView>(Resource.Id.ContactName);
+            var contactImage = view.FindViewById<ImageView>(Resource.Id.ContactImage);
+
+            contactName.Text = _contactList[position].DisplayName;
+
+            if (_contactList[position].PhotoId == null)
+            {
+
+                contactImage = view.FindViewById<ImageView>(Resource.Id.ContactImage);
+                contactImage.SetImageResource(Resource.Drawable.ContactImage);
+
+            }
+            else
+            {
+
+                var contactUri = ContentUris.WithAppendedId(ContactsContract.Contacts.ContentUri, _contactList[position].Id);
+                var contactPhotoUri = Android.Net.Uri.WithAppendedPath(contactUri, Contacts.Photos.ContentDirectory);
+
+                contactImage.SetImageURI(contactPhotoUri);
+            }
+            return view;
+        }
+        
+
+        void FillContacts()
+        {
+            var uri = ContactsContract.Contacts.ContentUri;
+
+            string[] projection = {
+                ContactsContract.Contacts.InterfaceConsts.Id,
+                ContactsContract.Contacts.InterfaceConsts.DisplayName,
+                ContactsContract.Contacts.InterfaceConsts.PhotoId
+            };
+
+           
+
+            _contactList = new List<Contact>();
+
+                    _contactList.Add(new Contact
+                    {
+                        Id = 1,
+                        DisplayName ="Raja",
+                        PhotoId = "Windows"
+                    });
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+            });
+
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+            });
+
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+            });
+
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+            });
+
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+            });
+
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+            });
+
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+            });
+
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+            });
+
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+
+            });
+
+            _contactList.Add(new Contact
+            {
+                Id = 1,
+                DisplayName = "Raja",
+                PhotoId = "Windows"
+            });
+
+        } 
+            }
+        
+
+        class Contact
+        {
+            public long Id { get; set; }
+
+            public string DisplayName { get; set; }
+
+            public string PhotoId { get; set; }
+        }
+    
+
+}
---
+++ HelloSwipeViewWithTabs/PageFragment.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using Android.Content;
+
+namespace HelloSwipeViewWithTabs
+{
+    public class PageFragment : Fragment
+    {
+        const string ARG_PAGE = "ARG_PAGE";
+        private int mPage;
+        ContactsAdapter contactsAdapter;
+        public static PageFragment newInstance(int page)
+        {
+            var args = new Bundle();
+            args.PutInt(ARG_PAGE, page);
+            var fragment = new PageFragment();
+            fragment.Arguments = args;
+            return fragment;
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            mPage = Arguments.GetInt(ARG_PAGE);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            if (mPage==1)
+            {
+                var view = inflater.Inflate(Resource.Layout.layout1, container, false);
+                Button b=  view.FindViewById<Button>(Resource.Id.button1);
+                b.Click += B_Click;
+                return view;
+            }
+            else
+            {
+                var view = inflater.Inflate(Resource.Layout.fragment_page, container, false);
+                contactsAdapter = new ContactsAdapter(this);
+                
+                var contactsListView = view.FindViewById<ListView>(Resource.Id.ContactsListView);
+                contactsListView.Adapter = contactsAdapter;
+                return view;
+            }
+          
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            Intent i = new Intent(this.Activity.ApplicationContext,typeof(Activity1));
+            StartActivity(i);
+        }
+    }
+}
